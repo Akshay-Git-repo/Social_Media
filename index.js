9c4fbb46ad6bdac51069ee97a463db9a6ca3f057
askharade123 <43643815+askharade123@users.noreply.github.com>
2020-07-05 10:35:28 +0530

    Added view engine

--- index.js
+++ index.js
@@ -2,6 +2,12 @@ const express=require("express");
 const app=express();
 const port=8000;
 
+//set an an view ejs
+
+app.set('view engine','ejs');
+app.set('views','./views');
+
+
 //use express router
 
 app.use('/', require('./routes'));
